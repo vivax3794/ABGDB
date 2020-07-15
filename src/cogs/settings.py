eccde3f0a9f640c318b7d2397ae16bd1b06ba409
vivax3794 <vivax3794@users.noreply.github.com>
2020-07-15 14:33:06 +0200

    added prefix_change

--- src/cogs/settings.py
+++ src/cogs/settings.py
@@ -9,8 +9,16 @@ class SettingCog(commands.Cog):
 
     @commands.command()
     async def prefix(self, ctx: commands.Context) -> None:
+        "return the current prefix"
         prefix = self.bot.db.prefix_get(ctx.guild.id)
         await ctx.send(f"my prefix is: `{prefix}`, but you can always just {self.bot.user.mention} if you are unsure :smile:")
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    async def prefix_change(self, ctx: commands.Context, new_prefix: str) -> None:
+        self.bot.db.prefix_update(ctx.guild.id, new_prefix)
+        await ctx.send(f"my prefix is now `{new_prefix}`")
 
 
 def setup(bot: Bot) -> None:
