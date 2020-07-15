50cf898f15f68020a760b2e245394320ec7a2cb7
vivax3794 <vivax3794@users.noreply.github.com>
2020-07-15 16:39:02 +0200

    added error handling

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,10 @@
-from .database import Database
+import traceback
 
 import discord
 from discord.ext import commands
 from loguru import logger
 
+from .database import Database
 from config import COGS
 
 
@@ -28,3 +29,41 @@ class Bot(commands.Bot):
 
     async def on_guild_join(self, guild: discord.Guild) -> None:
         self.db.prefix_add(guild.id, "!")
+
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.id == self.user.id:
+            logger.debug("not replying to my self")
+        elif message.guild is None:
+            await message.channel.send("sorry but I can not respond to commands in dms since I dont know what prefix to use.")
+        else:
+            await self.process_commands(message)
+
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+        tb = "".join(traceback.format_exception(None, error, error.__traceback__))
+        if ctx.command is not None:
+            logger.error(f"command {ctx.command.name} raised an error.")
+        else:
+            logger.error(error)
+
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send(f"{ctx.author.mention} command not found")
+            await ctx.send_help()
+
+        elif isinstance(error, commands.BadArgument) or isinstance(error, commands.MissingRequiredArgument):
+            logger.error("incorrect arguments given")
+            await ctx.send(f"{ctx.author.mention} invalid arguments")
+            await ctx.send_help(ctx.command)
+
+        elif isinstance(error, commands.MissingPermissions):
+            logger.error("missing permissions")
+            missing_permissions = "\n".join(f"* {permission}" for permission in error.missing_perms)
+            await ctx.send(f"{ctx.author.mention} you need these permissions to use this command: ```\n{missing_permissions}```")
+
+        else:
+            logger.error(f"unknow error\n{tb}")
+            error_embed = discord.Embed(
+                    color=discord.Color.red(),
+                    title=str(error),
+                    description=f"```\n{tb}```"
+                    )
+            await ctx.send(f"{ctx.author.mention} unknow error detected.", embed=error_embed)
--- src/cogs/settings.py
+++ src/cogs/settings.py
@@ -14,9 +14,11 @@ class SettingCog(commands.Cog):
         await ctx.send(f"my prefix is: `{prefix}`, but you can always just {self.bot.user.mention} if you are unsure :smile:")
 
     @commands.command()
-    @commands.guild_only()
     @commands.has_permissions(administrator=True)
     async def prefix_change(self, ctx: commands.Context, new_prefix: str) -> None:
+        """
+        change the current prefix.
+        """
         self.bot.db.prefix_update(ctx.guild.id, new_prefix)
         await ctx.send(f"my prefix is now `{new_prefix}`")
 
