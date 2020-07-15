52453c55e6d6037be6492bcdba52a1bb88925de0
vivax3794 <vivax3794@users.noreply.github.com>
2020-07-15 22:04:34 +0200

    added docstring to Spy to explain it's use

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,9 @@ from loguru import logger
 
 
 class Spy:
+    """
+    Logs attribute lookup on it self, used during debug and not in the actuall code.
+    """
     def __getattribute__(self, value: str) -> Any:
         if value.startswith("__") and value.endswith("__"):
             # ignore dunder methods
