b1ae32188b1f7ae91ecf2fc849ed83b1c7ff7a53
vivax3794 <vivax3794@users.noreply.github.com>
2020-07-21 20:58:12 +0200

    remove old settings commands

--- src/cogs/settings.py
+++ src/cogs/settings.py
@@ -7,23 +7,6 @@ class SettingsCog(commands.Cog, name="settings"):  # type: ignore
         super().__init__()
         self.bot = bot
 
-    @commands.command()
-    async def prefix(self, ctx: commands.Context) -> None:
-        "return the current prefix"
-        prefix = self.bot.db.prefix_get(ctx.guild.id)
-        await ctx.send(
-            f"my prefix is: `{prefix}`, but you can always just {self.bot.user.mention} if you are unsure :smile:"
-        )
-
-    @commands.command()
-    @commands.has_permissions(manage_guild=True)
-    async def change_prefix(self, ctx: commands.Context, new_prefix: str) -> None:
-        """
-        change the current prefix.
-        """
-        self.bot.db.prefix_update(ctx.guild.id, new_prefix)
-        await ctx.send(f"my prefix is now `{new_prefix}`")
-
 
 def setup(bot: Bot) -> None:
     bot.add_cog(SettingsCog(bot))
