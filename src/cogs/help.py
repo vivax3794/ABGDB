b279f719e65247d3325b262fd1a81b8c646f5f71
vivax3794 <vivax3794@users.noreply.github.com>
2020-07-21 16:09:19 +0200

    added help for commands

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -13,6 +13,13 @@ MAPPING = Dict[Optional[commands.Cog], List[commands.Command]]
 class HelpCommand(commands.HelpCommand, Spy):
     """show this"""
 
+    async def _send_embed(self, embed: discord.Embed) -> None:
+        """
+        Send an embed to the help destionation
+        """
+        destionation: discord.abc.Messageable = self.get_destination()
+        await destionation.send(embed=embed)
+
     async def send_bot_help(self, mapping: MAPPING) -> None:
         embed = discord.Embed(title="help", color=discord.Color.blue())
         for cog, command_list in mapping.items():
@@ -34,8 +41,16 @@ class HelpCommand(commands.HelpCommand, Spy):
                 inline=False
             )
 
-        channel: discord.abc.Messageable = self.get_destination()
-        await channel.send(embed=embed)
+        await self._send_embed(embed)
+
+    async def send_command_help(self, command: commands.Command) -> None:
+        embed = discord.Embed(
+                title=command.qualified_name,
+                description=f"```{self.get_command_signature(command)}```\n{command.help}",
+                color=discord.Color.blue()
+                )
+
+        await self._send_embed(embed)
 
 
 def setup(bot: Bot) -> None:
